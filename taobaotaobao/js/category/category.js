b4d216c43f2064fc75d5c78fa51ad0623b2e9088
feichong124 <444782155@qq.com>
2017-02-24 19:42:16 +0800

    taotao第一次推送

---
+++ taobaotaobao/js/category/category.js
@@ -0,0 +1,30 @@
+$(function() {
+	//	$.ajax({
+	//		type:"get",
+	//		//\n
+	//		//参照页面是category.html 这个页面
+	//		url:"./common.html",
+	//		async:true,
+	//		data:"",
+	//		dataType:"html",
+	//		//怎么获取html 内容
+	//		success:function(data){			 
+	//			var a = data.getElementsByClassName("common-fixFooterNav");
+	//			 console.log(a)
+	////			var  tempObj  = $(".common-fixNav").clone(true);
+	////			console.log(tempObj[0 ])
+	////			$(".common-fixNav").html(tempObj)
+	//		},
+	//		error:function(d){
+	//			console.log(d)
+	//		}
+	//		
+	//	});
+	//$(".zw-common-footer").load("../../world/html/common/common.html #zw-common-footer",function(x,y,z){});
+	$(".common-fixNav").load("./common.html .common-fixNav", function(a, b, c) {
+
+	})
+	$(".common-fixFooterNav").load("./common.html .common-fixFooterNav", function(a, b, c) {
+		console.log(b);
+	})
+}); //jq结尾
